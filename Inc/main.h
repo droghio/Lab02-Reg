4a9ebdbb21fc92fa1e9a97f5efb69d095a975f81
John Drogo <admin@jldapps.com>
2016-12-10 03:36:06 -0500

    Remove empty files, update readme.

--- Inc/main.h
+++
@@ -1,5 +0,0 @@
-  
-#ifndef __MAIN_H
-#define __MAIN_H
-#include "stm32f769xx.h"
-#endif /* __MAIN_H */
--- Src/stm32f7xx_it.c
+++
@@ -1,6 +0,0 @@
-#include "main.h"
-#include "stm32f7xx_it.h"
-
-void EXTI0_IRQHandler(void) {
-	//HAL_GPIO_EXTI_IRQHandler(USER_BUTTON_PIN);
-}
