a11e93fd73602e903845f95acfaf8efc795cdb01
John Drogo <admin@jldapps.com>
2016-12-10 02:29:56 -0500

    Removing unnecessary files.

--- SW4STM32/STM32F769I-Discovery/Debug/Example/User/subdir.mk
+++ SW4STM32/STM32F769I-Discovery/Debug/Example/User/subdir.mk
@@ -4,19 +4,13 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Projects/STM32F769I-Discovery/Examples/HAL/Lab02-Reg/Src/main.c \
-C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Projects/STM32F769I-Discovery/Examples/HAL/Lab02-Reg/Src/stm32f7xx_hal_timebase_tim.c \
-C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Projects/STM32F769I-Discovery/Examples/HAL/Lab02-Reg/Src/stm32f7xx_it.c 
+C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Projects/STM32F769I-Discovery/Examples/HAL/Lab02-Reg/Src/main.c 
 
 OBJS += \
-./Example/User/main.o \
-./Example/User/stm32f7xx_hal_timebase_tim.o \
-./Example/User/stm32f7xx_it.o 
+./Example/User/main.o 
 
 C_DEPS += \
-./Example/User/main.d \
-./Example/User/stm32f7xx_hal_timebase_tim.d \
-./Example/User/stm32f7xx_it.d 
+./Example/User/main.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
@@ -24,23 +18,7 @@ Example/User/main.o: C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU GCC Compiler'
 	@echo %cd%
-	arm-none-eabi-gcc -mcpu=cortex-m7 -mthumb -mfloat-abi=hard -mfpu=fpv5-d16 -DUSE_HAL_DRIVER -DSTM32F769xx -DUSE_STM32F769I_DISCO -I"C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Projects/STM32F769I-Discovery/Examples/HAL/Lab02-Reg/Inc" -I"C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Drivers/CMSIS/Device/ST/STM32F7xx/Include" -I"C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Utilities/Log" -I"C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Utilities/Fonts" -I"C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Utilities/CPU" -I"C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Drivers/CMSIS/Include"  -Os -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
-	@echo 'Finished building: $<'
-	@echo ' '
-
-Example/User/stm32f7xx_hal_timebase_tim.o: C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Projects/STM32F769I-Discovery/Examples/HAL/Lab02-Reg/Src/stm32f7xx_hal_timebase_tim.c
-	@echo 'Building file: $<'
-	@echo 'Invoking: MCU GCC Compiler'
-	@echo %cd%
-	arm-none-eabi-gcc -mcpu=cortex-m7 -mthumb -mfloat-abi=hard -mfpu=fpv5-d16 -DUSE_HAL_DRIVER -DSTM32F769xx -DUSE_STM32F769I_DISCO -I"C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Projects/STM32F769I-Discovery/Examples/HAL/Lab02-Reg/Inc" -I"C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Drivers/CMSIS/Device/ST/STM32F7xx/Include" -I"C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Utilities/Log" -I"C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Utilities/Fonts" -I"C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Utilities/CPU" -I"C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Drivers/CMSIS/Include"  -Os -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
-	@echo 'Finished building: $<'
-	@echo ' '
-
-Example/User/stm32f7xx_it.o: C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Projects/STM32F769I-Discovery/Examples/HAL/Lab02-Reg/Src/stm32f7xx_it.c
-	@echo 'Building file: $<'
-	@echo 'Invoking: MCU GCC Compiler'
-	@echo %cd%
-	arm-none-eabi-gcc -mcpu=cortex-m7 -mthumb -mfloat-abi=hard -mfpu=fpv5-d16 -DUSE_HAL_DRIVER -DSTM32F769xx -DUSE_STM32F769I_DISCO -I"C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Projects/STM32F769I-Discovery/Examples/HAL/Lab02-Reg/Inc" -I"C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Drivers/CMSIS/Device/ST/STM32F7xx/Include" -I"C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Utilities/Log" -I"C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Utilities/Fonts" -I"C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Utilities/CPU" -I"C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Drivers/CMSIS/Include"  -Os -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m7 -mthumb -mfloat-abi=hard -mfpu=fpv5-d16 -DSTM32F769xx -I"C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Projects/STM32F769I-Discovery/Examples/HAL/Lab02-Reg/Inc" -I"C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Drivers/CMSIS/Device/ST/STM32F7xx/Include" -I"C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Utilities/Log" -I"C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Utilities/Fonts" -I"C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Utilities/CPU" -I"C:/Users/John/Downloads/starting-pack/common/STM32Cube_FW_F7_V1.4.0/Drivers/CMSIS/Include"  -Os -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- Src/main.c
+++ Src/main.c
@@ -1,16 +1,17 @@
 #include "stm32f769xx.h"
+//#include "system_stm32f7xx.h"
 
+// Function Defines
 int Init_Tick();
+void TIM6_DAC_IRQHandler(void);
 
+// Code Body
 int main(void) {
-	/* STM32F7xx HAL library initialization:
-	 - Configure the Flash prefetch
-	 - Configure timer (TIM6) to generate an interrupt each 1 msec
-	 - Set NVIC Group Priority to 4
-	 - Low Level Initialization
-	 */
+	// Defined by CMSIS.
+	//System_Init();
 	Init_Tick();
 
+	// Enable GPIO clock?
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOJEN;
 	/* Delay after an RCC peripheral clock enabling */
 	asm ("nop");
@@ -20,6 +21,35 @@ int main(void) {
 	GPIOJ->MODER |= GPIO_MODER_MODER13
 			& (GPIO_MODER_MODER13 - (GPIO_MODER_MODER13 >> 1));
 
-	/* Insert a Delay of 1000 ms and toggle LED2, in an infinite loop */
 	while (1);
+}
+
+int Init_Tick(){
+	// Enable the global register.
+	// Looks like HAL hid this little gem, not this register isn't mentioned in
+	//   the STM32F7 ARM Reference Manual....
+	NVIC->ISER[TIM6_DAC_IRQn/32U] = (uint32_t)(1UL << (((uint32_t)TIM6_DAC_IRQn) & 0x1FUL));
+
+	// Enable TIM6 clock
+    RCC->APB1ENR |= RCC_APB1ENR_TIM6EN;
+    asm ( "nop" );
+    asm ( "nop" );
+
+    // Set pre-scaler to make a 1MHz ticker.
+    TIM6->PSC = (uint32_t) ((16000000U / 1000000U) - 1U);
+
+    // Set the Auto-reload Value
+    TIM6->ARR = (1000000U / 10U) - 1U;
+
+    // Trigger an Update Event.
+    TIM6->EGR = TIM_EGR_UG;
+
+    TIM6->DIER |= TIM_DIER_UIE;
+	TIM6->CR1 |= TIM_CR1_CEN;
+	return 0;
+}
+
+void TIM6_DAC_IRQHandler(void) {
+	TIM6->SR = ~TIM_DIER_UIE;
+	GPIOJ->ODR ^= ((uint16_t)0x2000U); //GPIO_PIN_13
 }
