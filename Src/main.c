d677ab09b799fed8dda65b20a5778e894e73d28e
John Drogo <admin@jldapps.com>
2016-12-10 02:47:21 -0500

    Last bit of code polish.

--- Src/main.c
+++ Src/main.c
@@ -1,15 +1,19 @@
-#include "stm32f769xx.h"
-//#include "system_stm32f7xx.h"
+// -- Imports ---------------
 
-// Function Defines
+// CMSIS Header, defines register structure.
+#include "stm32f769xx.h"
+// CMSIS Header, defines SystemInit function.
+#include "system_stm32f7xx.h"
+
+// -- Function Defines ------
 int Init_Tick();
 void TIM6_DAC_IRQHandler(void);
 
-// Code Body
+// -- Code Body -------------
 int main(void) {
 	// Defined by CMSIS.
-	//System_Init();
-	Init_Tick();
+	SystemInit();
+	Init_Timer();
 
 	// Enable GPIO clock?
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOJEN;
@@ -24,8 +28,8 @@ int main(void) {
 	while (1);
 }
 
-int Init_Tick(){
-	// Enable the global register.
+int Init_Timer(){
+	// Enable the global interrupt register.
 	// Looks like HAL hid this little gem, not this register isn't mentioned in
 	//   the STM32F7 ARM Reference Manual....
 	NVIC->ISER[TIM6_DAC_IRQn/32U] = (uint32_t)(1UL << (((uint32_t)TIM6_DAC_IRQn) & 0x1FUL));
@@ -35,13 +39,13 @@ int Init_Tick(){
     asm ( "nop" );
     asm ( "nop" );
 
-    // Set pre-scaler to make a 1MHz ticker.
-    TIM6->PSC = (uint32_t) ((16000000U / 1000000U) - 1U);
+    // Set pre-scaler to make a 10kHz ticker.
+    TIM6->PSC = (uint32_t) ((SystemCoreClock / 10000U) - 1U);
 
-    // Set the Auto-reload Value
-    TIM6->ARR = (1000000U / 10U) - 1U;
+    // Set the Auto-reload Value for 1Hz overflow
+    TIM6->ARR = (10000U / 1U) - 1U;
 
-    // Trigger an Update Event.
+    // Enable Update Interrupts.
     TIM6->EGR = TIM_EGR_UG;
 
     TIM6->DIER |= TIM_DIER_UIE;
@@ -50,6 +54,9 @@ int Init_Tick(){
 }
 
 void TIM6_DAC_IRQHandler(void) {
+	// Clear Interrupt Bit
 	TIM6->SR = ~TIM_DIER_UIE;
-	GPIOJ->ODR ^= ((uint16_t)0x2000U); //GPIO_PIN_13
+
+	//Toggle GPIO_PIN_13 (LED1)
+	GPIOJ->ODR ^= ((uint16_t)0x2000U);
 }
